be973e52fb40fa9700cbad8d54fe8dabdbf2dfbd
venomnert <neerthigan@gmail.com>
2017-01-14 23:04:58 -0500

    added dialog template

--- resource/js/action.js
+++ resource/js/action.js
@@ -1,39 +1,69 @@
+var dialog_nodes = document.querySelectorAll('dialog');
+var plus_nodes = document.querySelectorAll('.show');
+var close_nodes = document.querySelectorAll('.close');
+
+var dialog_array = [];
+var plus_array = [];
+var close_array = [];
+
 /*
+document.querySelector('#show').onclick = function () {
+  dialog.show();
+}
+document.querySelector('#close').onclick = function () {
+  dialog.close();
+}
+*/
 
-The JavaScript has one job: hide the current slide and show the next one.
-To accomplish this, we just have to change the classes of the slides in question.
-  -first get the slides
-  -onclick change the showing class to the child below
-  -if the child showing is the last one, move to the top and start over again
-
-var slider = document.getElementById('slides');
-var slides = document.querySelectorAll('ul#slides li');
-
-slider.addEventListener('click', function () {
-    var currKey = getCurrentSlide();
-    var currNode = slides.item(currKey);
-
-    if(currKey === slides.length-1) {
-      currKey = 0;
-    }
-    else {
-      currKey +=1;
-    }
-
-    if(currNode.classList.contains('showing')) {
-      currNode.classList.remove('showing');
-      slides.item(currKey).classList.add('showing');
-    }
-})
-
-function getCurrentSlide() {
-  var doc = slider.querySelector('li.showing');
-  var currKey = 0;
-
-  for(var key of slides.keys()){
-    if ( slides.item(key) === doc) {currKey = key;}
+function converNodeList(nodeList, arrayList) {
+  for (let i = 0; i < nodeList.length; i++) {
+    arrayList.push(nodeList.item(i));
   }
-  return currKey;
 }
 
-*/
+function assignDialogShow(dialog_array, plus_array) {
+  if (dialog_array.length !== plus_array.length) {
+    console.log('Unequal dialog for plus');
+    return;
+  }
+
+  for (let i = 0; i < plus_array.length; i++) {
+    let className = plus_array[i].classList.item(1);
+
+    for (let j = 0; j < dialog_array.length; j++) {
+      if (dialog_array[j].classList.contains(className)) {
+        plus_array[i].onclick = function () { dialog_array[j].show(); }
+      }
+    }
+  }
+}
+
+function assignDialogClose(dialog_array, close_array) {
+  if (dialog_array.length !== close_array.length) {
+    console.log('Unequal dialog for close');
+    return;
+  }
+
+  for (let i = 0; i < close_array.length; i++) {
+    let className = close_array[i].classList.item(1);
+
+    for (let j = 0; j < dialog_array.length; j++) {
+      if (dialog_array[j].classList.contains(className)) {
+        console.log(className, dialog_array[j]);
+        close_array[i].onclick = function () { dialog_array[j].close(); }
+      }
+    }
+  }
+}
+
+
+converNodeList(dialog_nodes, dialog_array);
+converNodeList(plus_nodes, plus_array);
+converNodeList(close_nodes, close_array);
+
+console.log(dialog_array);
+console.log(plus_array);
+console.log(close_array);
+
+assignDialogShow(dialog_array,plus_array);
+assignDialogClose(dialog_array,close_array);
